8b80a801b6574c28492af2f36baf6b15d9c455d2
null_ong2 <jannhyunsoo@gmail.com>
2026-02-10 00:46:58 +0900

    basic-101

---
+++ SocketIO/Robert/00-SocketIO101/basic.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const socketio = require('socket.io')
+
+app.use(express.static(__dirname + '/public'));
+
+const expressServer = app.listen(8000);
+const io = socketio(expressServer);
+
+console.log('Server is running on port 8000');
+
+io.on('connect', (socket) => {
+    console.log('Client connected', socket.id);
+    /**
+     * socket.emit is used to send a message to the client
+     * socket.on is used to listen for a message from the client
+     */
+    socket.emit('messageFromServer', { data: 'Welcome to the socket.io server!' });
+    socket.on('messageFromClient', (data) => { console.log(data); });
+});
