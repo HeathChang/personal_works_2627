c86cf036bec3ed50e2395eb88b8f2833b6a373f0
null_ong2 <jannhyunsoo@gmail.com>
2026-03-19 01:03:30 +0900

    phaser-intro-2

--- phaser/01-flappy-bird/src/index.js
+++ phaser/01-flappy-bird/src/index.js
@@ -8,11 +8,15 @@ const config = {
   height: 600,
   physics: {
     // Arcade physics plugin, manages physics simulation
-    default: 'arcade'
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
   },
   scene: {
     preload,
     create,
+    update,
   }
 }
 
@@ -20,14 +24,27 @@ const config = {
 function preload() {
   // scene context
   this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
 }
+let bird = null;
+let totalDelta = 0;
 
 // create: Creating game objects and setting up the game logic.
 function create() {
   // x, y, image key
-  this.add.image(config.width / 2, config.height / 2, 'sky');
+  this.add.image(0, 0, 'sky').setOrigin(0, 0);
+  // middle of height, 1/10 width
+  bird = this.physics.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0); // Sprite 객체(애니메이션 가능한 이미지)를 생성하는 API
 }
 
+function update(time, delta) {
+  totalDelta += delta;
+
+  if (totalDelta < 1000) { return; }
+
+  console.log(bird.body.velocity.y);
+  totalDelta = 0;
+}
 
 
 new Phaser.Game(config);
